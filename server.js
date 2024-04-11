744639e5e51c23d3b2fe030f26238dc7d84da550
Supanut <jimsupanut@gmail.com>
2024-04-12 03:13:24 +0700

    add query and params

--- server.js
+++ server.js
@@ -3,33 +3,51 @@ const express = require('express')
 const app = express()
 
 const todos = [
-    {id: 11, title : 'Learn HTML'},
-    {id: 22, title : 'Learn CSS'},
-    {id: 33, title : 'Learn Javascript'},
+    { id: 11, title: 'Learn HTML' },
+    { id: 22, title: 'Learn CSS' },
+    { id: 33, title: 'Learn Javascript' },
 ]
 
-app.get('/', (req,res) => {
-    res.json({msg : 'Welcome to Express Server'})
+app.get('/', (req, res) => {
+    res.json({ msg: 'Welcome to Express Server' })
 })
-app.get('/todos',(req,res) => {
+app.get('/todos', (req, res) => {
     res.json(todos)
 })
+app.get('/todos/search', (req, res) => {
+    const { keyword } = req.query
+    let output = todos.filter(el =>
+        el.title.toLowerCase().includes(keyword.toLowerCase()))
+    console.log(output)
+    res.json(req.output)
+})
+
 
 // แปลง code ให้หาจาก id ถ้าไม่มี msg : "have no data" , status เป็น 404
-app.get('/todos/:id', (req,res) => {
-    const {id} = req.params
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params
     console.log(id)
-    let output = todos.filter(el=>el.id === +id)
-    if(output.length <= 0) 
-        return res.status(404).json({msg : `have no data with id=${id}`})
+    let output = todos.filter(el => el.id === +id)
+    if (output.length <= 0)
+        return res.status(404).json({ msg: `have no data with id=${id}` })
     // res.send(todos[+req.params.index])
     res.json(output)
 })
 
-app.use((req,res)=> {
-    res.status(404).send({msg: 'resource not found'})
+
+app.get('/add/:x/:y', (req, res) => {
+    const { x, y } = req.params
+    console.log(req.params)
+    let sum = +x + +y
+    res.json({ sum: sum })
+})
+
+
+
+app.use((req, res) => {
+    res.status(404).send({ msg: 'resource not found' })
 })
 let port = process.env.PORT || 8000
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log("Server on port :", port)
 })
